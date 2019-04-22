0e9632895ede307fc6e5958b0a21deae4995772e
Grant Watson <gwatson117@gmail.com>
2019-04-22 18:00:02 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { DragDropContext } from 'react-dnd';
-import { Button } from 'antd';
 import { addBoard } from './actions/BoardActions.js';
 import HTML5Backend from 'react-dnd-html5-backend';
 import BoardsLayout from './components/BoardsLayout.js';
@@ -29,9 +28,9 @@ class App extends Component {
             ToDo Board
             <span style={{marginTop:"-4px", marginLeft:"2px", fontSize:"14px", display:"block"}}>by Grant Watson</span>
           </div>
-          <Button type="primary" style={{float:"right", position:"relative", display:"inline-block", right:"16%", top:"16px"}} onClick={this.addNewBoard}>
+          <div class="newBoard" style={{float:"right", position:"relative", display:"inline-block", right:"16%", top:"16px"}} onClick={this.addNewBoard}>
             New Board 
-          </Button>
+          </div>
         </div>
         <BoardsLayout boards={state.boards} dispatch={dispatch} cards={state.cards} /> 
       </div>
